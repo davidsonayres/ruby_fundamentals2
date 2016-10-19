faba7bf2d914384ee98f3e418f6eab0c2e6111df
Michael <mjames.ayres@gmail.com>
2016-10-19 13:19:23 -0400

    Create output method for grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,10 @@ grocery_list = ["bread", "cheese", "milk", "eggs", "bacon"]
 
 grocery_list << "rice"
 
-grocery_list.each do | item |
-  puts "* #{item}"
+def list(grocery_list)
+  grocery_list.each do | item |
+    puts "* #{item}"
+  end
 end
+
+list(grocery_list)
